956e11e13a826a8835a37d7d78c8677f13932c59
ryan <ryan.quivy@student.howest.be>
2024-10-24 15:34:27 +0200

    changed password stuff

--- dbUser.sql
+++ dbUser.sql
@@ -1,3 +1,4 @@
-CREATE USER editor@localhost IDENTIFIED BY 'editor';
-GRANT SELECT, INSERT, UPDATE, DELETE ON game_collections.* TO editor@localhost;
+CREATE USER 'editor'@'localhost' IDENTIFIED BY 'editor';
+GRANT SELECT, INSERT, UPDATE, DELETE ON game_collections.* TO 'editor'@'localhost';
+ALTER USER 'editor'@'localhost' IDENTIFIED WITH mysql_native_password BY 'editor';
 FLUSH PRIVILEGES;
